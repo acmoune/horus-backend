c4d42c3f650e4a020b1c123ce33221b551b5282b
Alain Moune <ac.moune@gmail.com>
2023-12-18 15:01:17 +0100

    Fixed postgres SSL setting

--- config/env/local/database.js
+++ config/env/local/database.js
@@ -12,7 +12,12 @@ module.exports = ({ env }) => {
         database,
         user,
         password,
-        ssl: { rejectUnauthorized: false },
+        ssl: env.bool('DATABASE_SSL', false) && {
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            false
+          ),
+        },
       },
       debug: false,
     },
